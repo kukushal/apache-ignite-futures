da1346c1d3097853ad336be71b31b621a4c3391d
Alexey Kukushkin <AlexeyKukushkin@yahoo.com>
2019-03-04 00:41:42 +0300

    save

---
+++ Apache.Ignite.Futures.Tests/TestObjects/CalculatorService2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Apache.Ignite.Core.Services;
+using Apache.Ignite.Futures.TopicMessage;
+
+namespace Apache.Ignite.Futures.Tests.TestObjects
+{
+    class CalculatorService2 : CalculatorService, ICalculator2
+    {
+        public TopicMessageFuture Sum(int n1, int n2, int duration)
+        {
+
+
+            var cancelSrc = new CancellationTokenSource();
+
+            var task = Sum(n1, n2, duration, cancelSrc.Token);
+
+            var fut = new TopicMessageFuture
+            {
+                Topic = Guid.NewGuid().ToString(),
+                State = State.Init
+            };
+
+            
+
+            task.ContinueWith(t =>
+            {
+
+            });
+
+            return fut;
+        }
+    }
+}
---
+++ Apache.Ignite.Futures.Tests/TestObjects/ICalculator2.cs
@@ -0,0 +1,11 @@
+﻿using Apache.Ignite.Futures.TopicMessage;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Apache.Ignite.Futures.Tests.TestObjects
+{
+    public interface ICalculator2
+    {
+        TopicMessageFuture Sum(int n1, int n2, int duration);
+    }
+}
